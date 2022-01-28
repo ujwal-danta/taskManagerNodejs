decdad82c8181a1fcafabbce1ec31d5092515e66
ujwal-danta <ujwaldanta@gmail.com>
2022-01-29 02:32:02 +0530

    c1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.get("/hello",(req,res)=>{
+    res.send("page running");
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("server running at port 3000")
+})
