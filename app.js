c36f079cba87bd47865e459efcb8f2f7e6c5ff7f
ujwal-danta <ujwaldanta@gmail.com>
2022-01-31 02:42:29 +0530

    n1

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks")
 
+
+//middleware
+app.use(express.json()); // to have the req data in req.body
+
+// routes
 app.get("/hello",(req,res)=>{
     res.send("page running");
 })
+
+app.use("/api/v1/tasks",tasks)
+
+
 
 
 
